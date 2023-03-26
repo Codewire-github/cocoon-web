bc6328728ab9ecac9a40e15c301f0c0be528edca
Codewire-github <ishanawal68@gmail.com>
2023-03-26 22:07:56 +0545

    Added landing page and its css file

---
+++ src/pages/landing_page/landing_page.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../../components/navbar/navbar";
+import "./landing_page.css";
+const LandingPage = () => {
+  return (
+    <div className="landing_page_container">
+      <NavBar />
+    </div>
+  );
+};
+
+export default LandingPage;
