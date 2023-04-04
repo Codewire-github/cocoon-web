efefadcbfe3a8ca06c792fe5a43cefe071dea981
Codewire-github <ishanawal68@gmail.com>
2023-04-04 20:59:03 +0545

    Added routes for pages and navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import LandingPage from "./pages/landing_page/landing_page";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/navbar/navbar";
+import ArticleDisplayPage from "./pages/article_display_page/article_display_page";
+import NewArticlePage from "./pages/new_article_page/newArticlepage";
+import Login from "./components/auth/Login";
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<LandingPage />} />
+          <Route path="/write-new-article" element={<NewArticlePage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
