ff7a8cc03f4dea26338f5e43ec9b62b1a1ac8ab3
Codewire-github <ishanawal68@gmail.com>
2023-04-09 21:09:02 +0545

    Added login page with user authentication

---
+++ src/components/auth/Login.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+//import { auth } from "./firebase";
+import logo from "../../images/cocoon.png";
+import sideImg from "../../images/undraw_completed_03xt.svg";
+import { UserAuth } from "../../context/authcontect";
+//import { userAuth } from "../../context/authcontect";
+import { useNavigate, Link } from "react-router-dom";
+import "./authpage.css";
+const Login = (props) => {
+  const navigate = useNavigate();
+
+  //const { User, logOut } = UserAuth();
+  const { googleSignIn, user, logOut } = UserAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleGoogleSignIn = async () => {
+    //await expressions are only allowed using async functions
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user !== null) {
+      navigate("/");
+    }
+  }, [user]);
+
+  return (
+    <div className="main-div">
+      <div className="row">
+        <div className="side-image">
+          <Link to="/">
+            <img className="logo-image" src={logo} alt="logo" />
+          </Link>
+          <img src={sideImg} className="side-img" alt="undraw-art" />
+          <div className="text">
+            <h1>Join the community of writers</h1>
+          </div>
+        </div>
+
+        <div className="form-container">
+          <form className="login-form">
+            <h2 className="login-greeting">Welcome back.</h2>
+            <p className="login-prompt">Please enter your details</p>
+            <div className="input-field">
+              <input
+                className="input-box"
+                defaultValue={email}
+                type="email"
+                placeholder="Enter your Email"
+                id="email"
+                name="email"
+                //onChange={(e) => setEmail(e.target.value)}
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div className="input-field">
+              <input
+                className="input-box"
+                defaultValue={password}
+                type="password"
+                placeholder="Enter your Password"
+                id="password"
+                name="password"
+                //onChange={(e) => setPassword(e.target.value)}
+                required
+                autoComplete="off"
+              />
+            </div>
+            {user?.displayName ? (
+              <button className="button" onClick={handleSignOut}>
+                Log out
+              </button>
+            ) : (
+              <button className="button" type="submit">
+                Log in
+              </button>
+            )}
+            <hr />
+            <div className="google-button">
+              <button onClick={handleGoogleSignIn}>
+                <img
+                  src={
+                    "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                  }
+                  className="google-logo"
+                  alt="google-logo"
+                />
+                Continue with google
+              </button>
+            </div>
+            <Link to="/signup" className="link-btn">
+              Don't have an account? <b>Create account</b>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
