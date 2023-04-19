149d21bfd0b690c0524346ffc8319803734d858d
Codewire-github <ishanawal68@gmail.com>
2023-04-19 19:40:41 +0545

    Added route to navigate to dynamic article page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import LandingPage from "./pages/landing_page/landing_page";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "./components/navbar/navbar";
 import ArticleDisplayPage from "./pages/article_display_page/article_display_page";
 import NewArticlePage from "./pages/new_article_page/newArticlepage";
 import Login from "./components/auth/Login";
@@ -19,6 +18,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/account" element={<Account />} />
+            <Route path="/post/:id" element={<ArticleDisplayPage />} />
           </Routes>
         </AuthContextProvider>
       </div>
