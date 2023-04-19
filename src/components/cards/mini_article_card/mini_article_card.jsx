9d6706f3d8c76c8f42ad0bfc6a65261d29f554f4
Codewire-github <ishanawal68@gmail.com>
2023-04-19 19:41:31 +0545

    Made minor UI changes for improvements

--- src/components/cards/mini_article_card/mini_article_card.jsx
+++ src/components/cards/mini_article_card/mini_article_card.jsx
@@ -1,21 +1,31 @@
 import "./mini-article-card.css";
 const MiniArticleCard = () => {
+  const authorImg =
+    "https://www.hakuhodo-global.com/wp_admin/wp-content/uploads/2017/11/Kazuhiro_Suda2square.jpg";
+
   const imgUrl =
     "https://duet-cdn.vox-cdn.com/thumbor/0x0:1774x1183/480x480/filters:focal(887x592:888x593):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/24539370/microsoftteamsavatars.jpg";
   return (
     <div className="mini-article-card-wrap">
-      <p className="genre">Tech</p>
       <section className="article-info">
-        <h2>
+        <span id="author-desc">
+          <img src={authorImg} alt="authorimg" className="author-img" />
+          <p id="author">Tom warren</p>
+        </span>
+        <h2 id="article-heading">
           Microsoft Teams now lets you transform into a 3D avatar during
           meetings
         </h2>
-        <span id="author-desc">
-          <p id="author">Tom warren</p>
-          <p id="time">An hour ago</p>
+        <p id="article-subheading">
+          ChatGPT recently passed the U.S. Medical Licensing Exam, but using it
+          for a real-world medical diagnosis would quickly turn deadly.
+        </p>
+        <span className="article-details">
+          <p id="published-date">May 26, 2022</p>
+          <p id="article-genre">Web Technology</p>
         </span>
       </section>
-      <img src={imgUrl} />
+      <img src={imgUrl} alt="article-photo" />
     </div>
   );
 };
