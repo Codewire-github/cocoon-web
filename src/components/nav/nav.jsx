90bc677454f5f0dcf0dacb2fbb3520407045159f
Codewire-github <ishanawal68@gmail.com>
2023-04-19 19:44:14 +0545

    Added userAuth and verification to navbar elements

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,10 +1,30 @@
-import "./navbar.css";
-import { Outlet } from "react-router-dom";
+import "./articlenavbar.css";
+import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { UserAuth } from "../../context/authcontect";
+import { useNavigate } from "react-router-dom";
+import { GreetingUser } from "../navbar/navbar";
 const Nav = () => {
+  const navigate = useNavigate();
+  const { logOut, user } = UserAuth();
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user == null) {
+      navigate("/");
+    }
+  }, [user]);
+
   return (
-    <div className="navbar_container">
+    <div className="singleblog_navbar_container">
       <h2>The cocoon</h2>
-      <ul className="nav-options-wrap">
+      <ul className="singleblog-nav-options-wrap">
         <li>Home</li>
         <p>/</p>
         <li>Explore</li>
@@ -12,8 +32,27 @@ const Nav = () => {
         <li>Community</li>
         <p>/</p>
         <li>About us</li>
-        <button className="nav-btn">Log in</button>
+        {user?.isAnonymous === false && (
+          <Link to="/writeNewArticle">
+            <button className="write-btn">Write</button>
+          </Link>
+        )}
+        {(user?.isAnonymous === true || user === null) && (
+          <Link to="/login">
+            <button className="nav-btn">Log in</button>
+          </Link>
+        )}
+        {user?.isAnonymous === false && (
+          <button className="nav-btn" onClick={handleSignOut}>
+            Log out
+          </button>
+        )}
       </ul>
+      <div>
+        {user?.isAnonymous === false && (
+          <GreetingUser userName={user?.displayName} userImg={user?.photoURL} />
+        )}
+      </div>
     </div>
   );
 };
