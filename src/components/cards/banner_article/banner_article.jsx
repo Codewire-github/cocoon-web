c14c06d159c77b7dc71fa0c24a68e387498a0082
NikitaKhuju <nikitashrestha345@gmail.com>
2023-04-20 19:06:31 +0545

    Changed the content of banner article card

--- src/components/cards/banner_article/banner_article.jsx
+++ src/components/cards/banner_article/banner_article.jsx
@@ -2,16 +2,19 @@ import "./banner-article.css";
 
 const BannerArticle = () => {
   const imgURL =
-    "https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1360/1200x960/filters:focal(1076x620:1077x621):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/24530056/DSCF0433.jpg";
+    "https://duet-cdn.vox-cdn.com/thumbor/308x0:3554x2223/1200x960/filters:focal(1880x391:1881x392):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/24590250/52822653443_0601982e08_o.jpg";
   return (
     <div className="banner-article-wrap">
       <img src={imgURL} alt="banner-img" />
       <section className="article-info">
-        <h1>Sonos Era 100 review: the new default smart speaker</h1>
+        <h1>
+          SpaceX Starship launch countdown: all of the news on its first test
+          flight
+        </h1>
         <p className="article-subheading">
-          The more affordable Era speaker improves upon the Sonos One by nearly
-          every measure and will likely find the same success and popularity
-          among Sonos customers.
+          SpaceX backed off of its first attempt at launching a Starship
+          prototype to orbital velocity, but it is scheduled to try again
+          Thursday morning.
         </p>
         <span>
           <p className="author">David Pierce</p>
