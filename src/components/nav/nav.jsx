77f0c2e0f96c1ab61af3a0c86e4864f36833c195
Codewire-github <ishanawal68@gmail.com>
2023-04-25 20:10:28 +0545

    Added route & dynamic styling acc. to bgcolor

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,8 +4,7 @@ import React, { useEffect } from "react";
 import { UserAuth } from "../../context/authcontect";
 import { useNavigate } from "react-router-dom";
 import { GreetingUser } from "../navbar/navbar";
-const Nav = () => {
-  const navigate = useNavigate();
+const Nav = ({ bgColor }) => {
   const { logOut, user } = UserAuth();
   const handleSignOut = async () => {
     try {
@@ -16,18 +15,54 @@ const Nav = () => {
   };
 
   return (
-    <div className="singleblog_navbar_container">
-      <h2>The cocoon</h2>
-      <ul className="singleblog-nav-options-wrap">
-        <li>Home</li>
+    <div
+      className="singleblog_navbar_container"
+      style={{
+        color: `${bgColor === "rgb(82 0 255)" ? "white" : "black"}`,
+      }}
+    >
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <h2
+          style={{
+            color: `${bgColor === "rgb(82 0 255)" ? "white" : "black"}`,
+          }}
+        >
+          The cocoon
+        </h2>
+      </Link>
+      <ul
+        className="singleblog-nav-options-wrap"
+        style={{
+          color: `${bgColor === "rgb(82 0 255)" ? "white" : "black"}`,
+        }}
+      >
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <li
+            style={{
+              color: `${bgColor === "rgb(82 0 255)" ? "white" : "black"}`,
+            }}
+          >
+            Home
+          </li>
+        </Link>
         <p>/</p>
-        <li>Explore</li>
+        <li
+          style={{
+            color: `${bgColor === "rgb(82 0 255)" ? "white" : "black"}`,
+          }}
+        >
+          Explore
+        </li>
         <p>/</p>
-        <li>Community</li>
-        <p>/</p>
-        <li>About us</li>
+        <li
+          style={{
+            color: `${bgColor === "rgb(82 0 255)" ? "white" : "black"}`,
+          }}
+        >
+          About us
+        </li>
         {user?.isAnonymous === false && (
-          <Link to="/writeNewArticle">
+          <Link to="/write-new-article">
             <button className="write-btn">Write</button>
           </Link>
         )}
