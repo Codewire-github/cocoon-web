43b20669fe921dec99814a19064c15e5b797c566
NikitaKhuju <nikitashrestha345@gmail.com>
2023-04-25 19:39:19 +0545

    Changed the path of write new article page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import "./App.css";
-import LandingPage from "./pages/landing_page/landing_page";
+import { AuthContextProvider } from "./context/authcontect";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "./database/firebase-config";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+import LandingPage from "./pages/landing_page/landing_page";
 import ArticleDisplayPage from "./pages/article_display_page/article_display_page";
 import NewArticlePage from "./pages/new_article_page/newArticlepage";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
 import Account from "./components/auth/Account";
-import { AuthContextProvider } from "./context/authcontect";
+import ArticlePage from "./pages/article_display_page_firebase/article_page";
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,11 +21,12 @@ function App() {
         <AuthContextProvider>
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/writeNewArticle" element={<NewArticlePage />} />
+            <Route path="/write-new-article" element={<NewArticlePage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/account" element={<Account />} />
             <Route path="/post/:id" element={<ArticleDisplayPage />} />
+            <Route path="/article/:id" element={<ArticlePage />} />
           </Routes>
         </AuthContextProvider>
       </div>
