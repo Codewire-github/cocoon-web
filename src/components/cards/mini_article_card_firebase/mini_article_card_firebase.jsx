f328ad7f587dfad30fe3c1575f62c85ad8942fbc
Codewire-github <ishanawal68@gmail.com>
2023-04-25 20:02:17 +0545

    Added miniarticlecard for data stored in firebase

---
+++ src/components/cards/mini_article_card_firebase/mini_article_card_firebase.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./mini-article-card.css";
+const MiniArticleCardFirebase = (props) => {
+  const authorImg =
+    "https://www.hakuhodo-global.com/wp_admin/wp-content/uploads/2017/11/Kazuhiro_Suda2square.jpg";
+
+  const imgUrl =
+    "https://duet-cdn.vox-cdn.com/thumbor/0x0:1774x1183/480x480/filters:focal(887x592:888x593):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/24539370/microsoftteamsavatars.jpg";
+  return (
+    <div className="mini-article-card-wrap">
+      <section className="article-info">
+        <span id="author-desc">
+          <img src={props.authorImg} alt="authorimg" className="author-img" />
+          <p id="author">{props.author}</p>
+        </span>
+        <Link
+          to={`/article/${props.linkid}`}
+          style={{ textDecoration: "none" }}
+        >
+          <h2 id="article-heading">{props.heading}</h2>
+        </Link>
+        <p id="article-subheading">{props.subheading}</p>
+        <span className="article-details">
+          <p id="published-date">{props.date}</p>
+          <p id="article-genre">{props.genre}</p>
+        </span>
+      </section>
+      <img src={props.imgUrl} alt="article-photo" />
+    </div>
+  );
+};
+
+export default MiniArticleCardFirebase;
