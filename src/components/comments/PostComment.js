bee149071d9d93331fbadee5d615f3d2fde6221e
Codewire-github <ishanawal68@gmail.com>
2023-04-25 20:13:51 +0545

    Moved to comments folder

---
+++ src/components/comments/PostComment.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../../database/firebase-config";
+//import { useNavigate } from "react-router-dom";
+
+const PostComment = () => {
+  const [comment, setComment] = useState("");
+  const commentsCollectionRef = collection(db, "comments");
+  //let navigate = useNavigate();
+
+  const postComment = async () => {
+    await addDoc(commentsCollectionRef, {
+      comment,
+      //author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
+    });
+    //navigate("/");
+  };
+
+  /*useEffect(() => {
+        if (!isAuth) {
+        navigate("/login");
+      }
+      }, []);
+      */
+
+  return (
+    <form className="commentBox">
+      <input
+        type="text"
+        className="postInput"
+        placeholder="Add a Comment..."
+        onChange={(event) => {
+          setComment(event.target.value);
+        }}
+      />
+      <button className="postButton" onClick={postComment}>
+        Post
+      </button>
+    </form>
+  );
+};
+
+export default PostComment;
