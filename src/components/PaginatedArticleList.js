9aad797950c77ce0e3c7532481ea67f978c3cd78
Codewire-github <ishanawal68@gmail.com>
2023-05-01 12:52:29 +0545

    Imported MiniArticleCard

--- src/components/PaginatedArticleList.js
+++ src/components/PaginatedArticleList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase";
-import "./PaginatedArticleList.css"
-
+import { db } from "../database/firebase-config";
+import "./PaginatedArticleList.css";
+import MiniArticleCardFirebase from "./cards/mini_article_card_firebase/mini_article_card_firebase";
+import { NumtoMonth } from "./numtomonth";
 const PAGE_SIZE = 10;
 
 const PaginatedArticleList = () => {
@@ -24,7 +25,7 @@ const PaginatedArticleList = () => {
   }, []);
   //determine the pages to be displayed.
   const totalPages = Math.ceil(articles.length / PAGE_SIZE);
-  
+
   const startIndex = page * PAGE_SIZE;
   const endIndex = startIndex + PAGE_SIZE;
   const visibleArticles = articles.slice(startIndex, endIndex);
@@ -42,19 +43,22 @@ const PaginatedArticleList = () => {
       <h1>Trending Articles</h1>
       {visibleArticles.map((article) => (
         <div className="articleContainer" key={article.id}>
-          <div className="articleInfo">
-              <h3>{article.authorName}</h3>
-              <h4>{article.published_date}</h4>
-              <h5>{article.genre}</h5>
-          </div>
-          <h2>{article.title}</h2>
+          <MiniArticleCardFirebase
+            linkid={article.id}
+            author={article.authorName}
+            authorImg={article.authorImgURL}
+            heading={article.title}
+            subheading={article.sub_description}
+            imgUrl={article.img_address}
+            date={`${NumtoMonth(article.published_date[1])} ${
+              article.published_date[0]
+            }, ${article.published_date[2]}`}
+            genre={article.genre}
+          />
         </div>
       ))}
       <div className="pagination">
-        <button
-          onClick={handlePrevPageClick}
-          disabled={page === 0}
-        >
+        <button onClick={handlePrevPageClick} disabled={page === 0}>
           Prev Page
         </button>
         {Array.from({ length: totalPages }).map((_, index) => (
