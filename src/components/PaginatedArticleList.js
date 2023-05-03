99025e095fca083e8016440829aa12c4081b4bb0
Codewire-github <ishanawal68@gmail.com>
2023-05-03 17:29:06 +0545

    Added listing article acc. to selected genre

--- src/components/PaginatedArticleList.js
+++ src/components/PaginatedArticleList.js
@@ -6,7 +6,7 @@ import MiniArticleCardFirebase from "./cards/mini_article_card_firebase/mini_art
 import { NumtoMonth } from "./numtomonth";
 const PAGE_SIZE = 10;
 
-const PaginatedArticleList = () => {
+const PaginatedArticleList = ({ current_genre }) => {
   const [page, setPage] = useState(0);
   const [articles, setArticles] = useState([]);
 
@@ -28,8 +28,15 @@ const PaginatedArticleList = () => {
 
   const startIndex = page * PAGE_SIZE;
   const endIndex = startIndex + PAGE_SIZE;
-  const visibleArticles = articles.slice(startIndex, endIndex);
-
+  const selectedGenreAritcles = articles.filter(
+    (article) => article.genre === current_genre
+  );
+  let visibleArticles;
+  if (current_genre === "") {
+    visibleArticles = articles.slice(startIndex, endIndex) || [];
+  } else {
+    visibleArticles = selectedGenreAritcles.slice(startIndex, endIndex) || [];
+  }
   const handlePrevPageClick = () => {
     setPage((prevPage) => prevPage - 1);
   };
@@ -40,26 +47,31 @@ const PaginatedArticleList = () => {
 
   return (
     <div className="paginatedArticleList">
-      <h1>Trending Articles</h1>
-      {visibleArticles.map((article) => (
-        <div className="articleContainer" key={article.id}>
-          <MiniArticleCardFirebase
-            linkid={article.id}
-            author={article.authorName}
-            authorImg={article.authorImgURL}
-            heading={article.title}
-            subheading={article.sub_description}
-            imgUrl={article.img_address}
-            date={`${NumtoMonth(article.published_date[1])} ${
-              article.published_date[0]
-            }, ${article.published_date[2]}`}
-            genre={article.genre}
-          />
-        </div>
-      ))}
+      <div>
+        {visibleArticles.length === 0 ? (
+          <EmptyCard message="You may want to go back and try a keyword?" />
+        ) : (
+          visibleArticles.map((article) => (
+            <div className="articleContainer" key={article.id}>
+              <MiniArticleCardFirebase
+                linkid={article.id}
+                author={article.authorName}
+                authorImg={article.authorImgURL}
+                heading={article.title}
+                subheading={article.sub_description}
+                imgUrl={article.img_address}
+                date={`${NumtoMonth(article.published_date[1])} ${
+                  article.published_date[0]
+                }, ${article.published_date[2]}`}
+                genre={article.genre}
+              />
+            </div>
+          ))
+        )}
+      </div>
       <div className="pagination">
         <button onClick={handlePrevPageClick} disabled={page === 0}>
-          Prev Page
+          <i className="fas fa-chevron-left"></i>
         </button>
         {Array.from({ length: totalPages }).map((_, index) => (
           <button
@@ -74,7 +86,7 @@ const PaginatedArticleList = () => {
           onClick={handleNextPageClick}
           disabled={page === totalPages - 1}
         >
-          Next Page
+          <i className="fas fa-chevron-right"></i>
         </button>
       </div>
     </div>
@@ -82,3 +94,25 @@ const PaginatedArticleList = () => {
 };
 
 export default PaginatedArticleList;
+
+export const EmptyCard = (props) => {
+  return (
+    <div
+      style={{
+        padding: "2rem 3rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "1rem",
+      }}
+    >
+      <i
+        className="far fa-envelope-open"
+        style={{ color: "grey", fontSize: "35px" }}
+      ></i>
+      <h1 style={{ color: "black", fontSize: "20px" }}>No Articles Found</h1>
+      <p style={{ color: "grey" }}>{props.message}</p>
+      <p></p>
+    </div>
+  );
+};
