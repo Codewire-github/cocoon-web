3265050ed1f290f9061eedf30cd0ad78a80fa5f6
Codewire-github <ishanawal68@gmail.com>
2023-05-03 17:33:34 +0545

    Minor changes

--- src/components/custom-components/customButtons.jsx
+++ src/components/custom-components/customButtons.jsx
@@ -4,7 +4,7 @@ export const OptionButton = styled.button`
   border-radius: 8px;
   border: 1.5px solid black;
   padding: 0.5rem;
-  margin: 0rem 0.5rem 0rem 0rem;
+  margin: 0rem 0.5rem 0.5rem 0rem;
   min-width: 2.4rem;
   min-height: 1.5rem;
   font-size: 1.1rem;
