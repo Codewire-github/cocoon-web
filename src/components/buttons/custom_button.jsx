e900b7ac74d26114f9ad4745b9a223676110376a
Codewire-github <ishanawal68@gmail.com>
2023-05-03 17:29:42 +0545

    Made some UI improvements

--- src/components/buttons/custom_button.jsx
+++ src/components/buttons/custom_button.jsx
@@ -4,7 +4,9 @@ export const CButton = styled.button`
   background-color: black;
   color: white;
   padding: 0.8rem 1.5rem;
+  padding-bottom: 10px;
   font-size: 1.1rem;
+  margin: 10px;
   font-weight: bold;
   border: none;
   border-radius: 0.8rem;
