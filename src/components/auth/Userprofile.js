4e42d9b5b6f26375f31f116dbdcb0572b8210602
Codewire-github <ishanawal68@gmail.com>
2023-05-06 17:15:01 +0545

    Changed the article pulling method

--- src/components/auth/Userprofile.js
+++ src/components/auth/Userprofile.js
@@ -1,28 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { UserAuth } from "../../context/authcontect";
 import { useNavigate } from "react-router-dom";
-import { db } from "../../database/firebase-config";
-import { collection, getDocs } from "firebase/firestore";
 import { NumtoMonth } from "../numtomonth";
 
 import "./Userprofile.css";
 import MiniArticleCardFirebase from "../cards/mini_article_card_firebase/mini_article_card_firebase";
 import { EmptyCard } from "../PaginatedArticleList";
+import GetArticleCollection from "../../database/article_collection";
+import Nav from "../nav/nav";
 
 export default function Profile() {
   const navigate = useNavigate();
 
-  const { logOut, user } = UserAuth();
-
-  const [articles, setArticles] = useState([]);
-  const articlesCollectionRef = collection(db, "articles");
-  useEffect(() => {
-    const getArticles = async () => {
-      let data = await getDocs(articlesCollectionRef);
-      setArticles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getArticles();
-  }, []);
+  const { user } = UserAuth();
+  const articles = GetArticleCollection() || [];
 
   useEffect(() => {
     if (user == null) {
@@ -40,60 +31,63 @@ export default function Profile() {
   }
 
   return (
-    <div className="fields">
-      <div className="userIntro">
-        <p className="profile-txt">
-          <b>User Profile</b>
-        </p>
-        <img src={user?.photoURL} alt="Avatar" className="useravatar" />
-        <p className="userkoname"> {user?.displayName}</p>
-        <p className="writer-txt">Writer</p>
-      </div>
-
-      <div className="mid-contain">
-        <div className="uploads-no">
-          <p className="num">
-            <b>
-              {userUploads.length < 10
-                ? `0${userUploads.length}`
-                : userUploads.length}
-            </b>
+    <div style={{ backgroundColor: "white" }}>
+      <Nav bgColor="white" />
+      <div className="fields">
+        <div className="userIntro">
+          <p className="profile-txt">
+            <b>User Profile</b>
           </p>
-          <p className="uploads-txt">Uploads</p>
+          <img src={user?.photoURL} alt="Avatar" className="useravatar" />
+          <p className="userkoname"> {user?.displayName}</p>
+          <p className="writer-txt">Writer</p>
         </div>
-        <div className="likes-no">
-          <p className="num">
-            <b>{totalLikes < 10 ? `0${totalLikes}` : totalLikes}</b>
-          </p>
-          <p className="likes-txt">Likes</p>
-        </div>
-      </div>
 
-      <div className="user-container">
-        <div className="heading-contain">
-          <h3 className="uploads-heading">My uploads</h3>
+        <div className="mid-contain">
+          <div className="uploads-no">
+            <p className="num">
+              <b>
+                {userUploads.length < 10
+                  ? `0${userUploads.length}`
+                  : userUploads.length}
+              </b>
+            </p>
+            <p className="uploads-txt">Uploads</p>
+          </div>
+          <div className="likes-no">
+            <p className="num">
+              <b>{totalLikes < 10 ? `0${totalLikes}` : totalLikes}</b>
+            </p>
+            <p className="likes-txt">Likes</p>
+          </div>
         </div>
-        <div className="uploads-des">
-          {userUploads.length === 0 ? (
-            <EmptyCard message="You may want to upload an article?" />
-          ) : (
-            userUploads.map((article) => (
-              <div key={article.id}>
-                <MiniArticleCardFirebase
-                  linkid={article.id}
-                  author={article.authorName}
-                  authorImg={article.authorImgURL}
-                  heading={article.title}
-                  subheading={article.sub_description}
-                  imgUrl={article.img_address}
-                  date={`${NumtoMonth(article.published_date[1])} ${
-                    article.published_date[0]
-                  }, ${article.published_date[2]}`}
-                  genre={article.genre}
-                />
-              </div>
-            ))
-          )}
+
+        <div className="user-container">
+          <div className="heading-contain">
+            <h3 className="uploads-heading">My uploads</h3>
+          </div>
+          <div className="uploads-des">
+            {userUploads.length === 0 ? (
+              <EmptyCard message="You may want to upload an article?" />
+            ) : (
+              userUploads.map((article) => (
+                <div key={article.id}>
+                  <MiniArticleCardFirebase
+                    linkid={article.id}
+                    author={article.authorName}
+                    authorImg={article.authorImgURL}
+                    heading={article.title}
+                    subheading={article.sub_description}
+                    imgUrl={article.img_address}
+                    date={`${NumtoMonth(article.published_date[1])} ${
+                      article.published_date[0]
+                    }, ${article.published_date[2]}`}
+                    genre={article.genre}
+                  />
+                </div>
+              ))
+            )}
+          </div>
         </div>
       </div>
     </div>
