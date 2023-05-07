b1e9bd0cc658e835dcde655fc6dc95e0633921c4
Codewire-github <ishanawal68@gmail.com>
2023-05-07 18:05:24 +0545

    Changed data retrival method

--- src/database/article_collection.js
+++ src/database/article_collection.js
@@ -1,23 +1,34 @@
 import { useEffect, useState } from "react";
-import React from "react";
+
 import { db } from "./firebase-config";
-import { doc } from "firebase/firestore";
+
 import { getDocs } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 
-const GetArticleCollection = () => {
-  const [articles, setArticles] = useState([]);
+/* useEffect(() => {
+      const getArticles = async () => {
+        let data = await getDocs(articlesCollectionRef);
+        setArticles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+      getArticles();
+    }, []);
+  
+    return articles;
+  }; */
 
+const fetchData = async () => {
   const articlesCollectionRef = collection(db, "articles");
-
-  useEffect(() => {
-    const getArticles = async () => {
-      let data = await getDocs(articlesCollectionRef);
-      setArticles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getArticles();
-  }, []);
-
-  return articles;
+  const querySnapshot = await getDocs(articlesCollectionRef);
+  const data = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  return data;
 };
-export default GetArticleCollection;
+
+export let ArticlesCollection = [];
+
+fetchData()
+  .then((data) => {
+    ArticlesCollection = data;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
