911017dbc38b2c2c032bbdadcf4f8c45ff2eeb12
Codewire-github <ishanawal68@gmail.com>
2023-05-07 18:06:06 +0545

    Created sidenav for mobile view

---
+++ src/components/sidenavbar/sidenav.jsx
@@ -0,0 +1,87 @@
+import "./sidenav.css";
+import cocoonLogo from "../../images/cocoonLogo.png";
+import { Link } from "react-router-dom";
+import React from "react";
+import { UserAuth } from "../../context/authcontect";
+
+const SideNavbar = ({ toggleSidenav, isShown }) => {
+  const { logOut, user } = UserAuth();
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div
+      className="sidenav_container"
+      style={{ right: isShown ? "4%" : "-100%" }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Link to="/" className="Logo" style={{ textDecoration: "none" }}>
+          <img src={cocoonLogo} alt="Logo" />
+          <h2>The cocoon</h2>
+        </Link>
+        <button
+          style={{
+            color: "rgb(82 0 255)",
+            background: "none",
+            border: "none",
+            cursor: "pointer",
+            fontSize: "30px",
+          }}
+          onClick={toggleSidenav}
+        >
+          <i className="fas fa-times"></i>
+        </button>
+      </div>
+      <ul className="sidenav-options-wrap">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <li>Home</li>
+        </Link>
+
+        <Link to="/explore" style={{ textDecoration: "none" }}>
+          <li>Explore</li>
+        </Link>
+
+        <Link to="/aboutus" style={{ textDecoration: "none" }}>
+          <li>About us</li>
+        </Link>
+
+        {user?.isAnonymous === false && (
+          <Link to="/write-new-article">
+            <button className="write-btn">Write</button>
+          </Link>
+        )}
+        {(user?.isAnonymous === true || user === null) && (
+          <Link to="/login">
+            <button className="nav-btn">Log in</button>
+          </Link>
+        )}
+        {user?.isAnonymous === false && (
+          <button className="nav-btn" onClick={handleSignOut}>
+            Log out
+          </button>
+        )}
+      </ul>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: "1rem",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default SideNavbar;
