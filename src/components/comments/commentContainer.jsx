a60661b4dab189b0ddac4742172631d77a3d37da
Codewire-github <ishanawal68@gmail.com>
2023-05-07 18:11:19 +0545

    Added animatio to comment section opening

--- src/components/comments/commentContainer.jsx
+++ src/components/comments/commentContainer.jsx
@@ -1,13 +1,20 @@
+import { isPlainObject } from "@tiptap/react";
 import { UserAuth } from "../../context/authcontect";
 import PostComment from "./PostComment";
 import "./commentContainer.css";
 import DisplayComments from "./displayComments";
-const CommentContainer = ({ handleOpenOverlay, bgColor }) => {
+const CommentContainer = ({ handleOpenOverlay, bgColor, isOpened }) => {
   const { user } = UserAuth();
 
   return (
-    <div className="comment-main-container">
-      <div className="comment-overlay-container">
+    <div
+      className="comment-main-container"
+      style={{ right: isOpened ? "0%" : "-100%", opacity: isOpened ? 1 : 0.4 }}
+    >
+      <div
+        className="comment-overlay-container"
+        style={{ right: isOpened ? "0%" : "-100%" }}
+      >
         <section
           className="comment-overlay-heading"
           style={{
