51dc9c0dd35cec8b67a9555906153119230d4410
Codewire-github <ishanawal68@gmail.com>
2023-05-13 19:28:51 +0545

    Changed the data to fetch on every page load

--- src/components/auth/Userprofile.js
+++ src/components/auth/Userprofile.js
@@ -1,25 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { UserAuth } from "../../context/authcontect";
 import { useNavigate } from "react-router-dom";
 import { NumtoMonth } from "../numtomonth";
-
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../database/firebase-config";
 import "./Userprofile.css";
 import MiniArticleCardFirebase from "../cards/mini_article_card_firebase/mini_article_card_firebase";
 import { EmptyCard } from "../PaginatedArticleList";
 import Nav from "../nav/nav";
-
+import { CheckCurrentYear } from "../../pages/landing_page/landing_page";
 export default function Profile({ articlesCollection }) {
   const navigate = useNavigate();
 
   const { user } = UserAuth();
+  const [articles, setArticles] = useState([]);
 
+  useEffect(() => {
+    const getArticles = async () => {
+      const articlesCollectionRef = collection(db, "articles");
+      let data = await getDocs(articlesCollectionRef);
+      setArticles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getArticles();
+  }, []);
   useEffect(() => {
     if (user == null) {
       navigate("/");
     }
   }, [user]);
 
-  const userUploads = articlesCollection.filter(
+  const userUploads = articles.filter(
     (article) => article.userID === user?.uid
   );
 
@@ -86,7 +96,7 @@ export default function Profile({ articlesCollection }) {
                     imgUrl={article.img_address}
                     date={`${NumtoMonth(article.published_date[1])} ${
                       article.published_date[0]
-                    }, ${article.published_date[2]}`}
+                    }${CheckCurrentYear(article.published_date[2])}`}
                     genre={article.genre}
                     likes={article.likes.length}
                     readTime={calculateReadTime(article.article_description)}
