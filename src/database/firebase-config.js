d5bd6d9a5372168e0a92040ca4ba3ddddb418d6f
Codewire-github <ishanawal68@gmail.com>
2024-06-28 16:46:25 +0545

    Added firebase config after fixing

---
+++ src/database/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
+// Web app's Firebase configuration
+const firebaseConfig = firebaseSensitiveConfigs;
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+//Firebase api for storing and retriving articles
